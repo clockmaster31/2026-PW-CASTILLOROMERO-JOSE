d4b42dde184003192c114f167abc6a13ca3b4d58
clockmaster31 <jcastillor2401@alumno.ipn.mx>
2026-09-15 19:56:49 -0600

    por que no jala...

--- primer parcial/validaciones/js/validaciones.js
+++ primer parcial/validaciones/js/validaciones.js
@@ -13,45 +13,37 @@ Las expresiones regulares, son patrones que nos ayudan a validar cadenas bajo ci
 */ 
 
 const patrones = {
-    nombre : /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü\s]{2,60}$/,
-    boleta : /^\d{10}$/,
-    fecha : /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
-
+    nombre: /^[A-Za-zÁÉÍÓÚÑáéíóúÜü\s]{2,60}$/,
+    boleta: /^\d{10}$/,
+    fecha: /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
 };
 
 const mensajes = {
-    nombre : "Solo letras y espacios, entre 2 y 60 caracteres.",
-    boleta : "Debe tener exactamente 10 dígitos numéricos.",
-    fecha : "La fecha debe tener el formato DD/MM/AAAA."
-};
-
-function validarCampo(campo, valor){
-    return patrones[campo].test(valor.trim())
+    nombre: "Solo letra u espacios, entre 2 y 60 caracteres.",
+    boleta: "Debe tener exactamente 10 digitos numericos",
+    fecha: "La fecha debe tener el formato DD/MM/AAAA"
 }
 
-//para validar el formulario debemos ocupar los principios de obtención y manipulación de los elementos del DOM 
+function validarCampo(campo, valor) {
+    return patrones[campo].test(valor.trim());
+}
+
+// Para validar el formulario debemos ocupar los principios de obtencion y manipulacion de los elementos del DOM
 
 if (typeof document !== 'undefined') {
     const formulario = document.getElementById('form-registro');
-
     formulario.addEventListener('submit', (evento) => {
-        evento.preventDefault(); // Evita que el formulario se envíe automáticamente
-
+        evento.preventDefault(); //Evita que el formilario se envie automaticamente
         let formularioValido = true;
-
         for(const campo of Object.keys(patrones)){
             const input = document.getElementById(campo);
-            const errorSpan = document.getElementById(`error-${campo}`);
+            const spanError = document.getElementById(`error-${campo}`);
             const esValido = validarCampo(campo, input.value);
             input.classList.toggle('invalido', !esValido);
             spanError.textContent = esValido ? '' : mensajes[campo];
-            if(!esValido) formularioValido = false;
+            if (!esValido) formularioValido = false;
         }
-
         const mensajeExito = document.getElementById('mensaje-exito');
-        mensajeExito.textContent = formularioValido ? 'Registro exitoso!' : '';
-        
-
-
-    });
+        mensajeExito.textContent = formularioValido ? 'Registro exitoso!' : '';       
+    })
 }
